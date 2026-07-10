5fa9ec4ac215d152b773360bba2b85e1a9653572
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2026-07-10 07:50:25 -0300

    finish slices2

--- exercises/slices/slices2/main.go
+++ exercises/slices/slices2/main.go
@@ -1,13 +1,13 @@
 // slices2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
 	names := [4]string{"John", "Maria", "Carl", "Peter"}
-	lastTwoNames := names[] // after figuring out the answer, try with other low/high bounds
+	totalNames := len(names)
+	lastTwoNames := names[totalNames-2 : totalNames] // after figuring out the answer, try with other low/high bounds
 	fmt.Println(lastTwoNames)
 }
